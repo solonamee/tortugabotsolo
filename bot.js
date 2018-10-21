6a073d1614207db5a7fda80ecffb2f4591296842
solonamee <44318489+solonamee@users.noreply.github.com>
2018-10-21 15:57:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,7 +107,7 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(ayarlar.token);
+client.login(process.env.BOT_TOKEN);
 
 const { GOOGLE_API_KEY } = require('./anahtarlar.json');
 const YouTube = require('simple-youtube-api');
